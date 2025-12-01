9751abf85e8394cb5fe5d63b1f8fad1020ae376b
YoXia <yongyou0604@yahoo.com>
2025-12-01 18:36:42 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -20,25 +20,17 @@ var init = function (window) {
         ///////////////////
         
         // TODO 1 : Declare and initialize our variables
-        var circle
-        var circles = []
+        
 
 
         // TODO 2 : Create a function that draws a circle 
-        function drawCircle() {
-            circle = draw.randomCircleInArea(canvas, true, true, "#999", 2);
-            physikz.addRandomVelocity(circle, canvas, 5, 5);
-            view.addChild(circle);
-            circles.push(circle);
-        }
+        
 
 
         // TODO 3 : Call the drawCircle() function
         
         // TODO 7 : Use a loop to create multiple circles
-        for (var i = 0; i < 100; i++) {
-            drawCircle()
-        }
+        
 
 
 
@@ -51,16 +43,7 @@ var init = function (window) {
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-        function update() {
-            // TODO 4 : Update the position of each circle using physikz.updatePosition()
-            
-            // TODO 5 : Call game.checkCirclePosition() on your circles
-           
-            // TODO 8 / TODO 9 : Iterate over the array
-           for (i = 0; i < circles.length; i++)
-           physikz.updatePosition(circles[i]) 
-           game.checkCirclePosition(circles[i])
-        }
+        
     
         /* 
         This Function should check the position of a circle that is passed to the 
@@ -75,15 +58,7 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            if (circle.x < 0) {
-                circle.x = circle.width
-            }
-            if (circle.y < 0) {
-                circle.y = circle.height
-            }
-            if (circle.y > circle.height) {
-                circle.y = 0
-            }
+            
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
